400e7d066773a3034c9f781ae739c88ecfa8348b
Victor Nieto <vmnieto2@gmail.com>
2025-04-24 17:45:13 -0500

    eliminando clases que no se usaran en este proyecto

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from Config.db import BASE, engine
 from Middleware.get_json import JSONMiddleware
-from Router.User import user_router
+# from Router.User import user_router
 from Router.Parametros import parametros_router
-from Router.Formacion import formacion_router
+# from Router.Formacion import formacion_router
 
 app = FastAPI()
 app.title = "Avantika Solicitud Cotizaciones a Compras"
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_headers=["*"],  # Permitir todos los encabezados; puedes especificar los encabezados permitidos.
 )
 app.include_router(parametros_router)
-app.include_router(user_router)
-app.include_router(formacion_router)
+# app.include_router(user_router)
+# app.include_router(formacion_router)
 
 BASE.metadata.create_all(bind=engine)
---
+++ querys_nuevo_proyecto.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `solicitudes_compras` ( 
+    `id` BIGINT NOT NULL AUTO_INCREMENT, 
+    `negociador` VARCHAR(50), 
+    `cuerpo_texto` TEXT, 
+    `usuario_creador_solicitud` VARCHAR(50) NOT NULL, 
+    `estado` TINYINT(1) NOT NULL DEFAULT 1, 
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(), 
+    PRIMARY KEY (`id`) 
+); 
+
+CREATE TABLE `solicitudes_compras_detalles` ( 
+    `id` BIGINT NOT NULL AUTO_INCREMENT, 
+    `solicitud_id` BIGINT NOT NULL, 
+    `referencia` VARCHAR(50), 
+    `producto` TEXT, 
+    `cantidad` INT(10), 
+    `proveedor` TEXT, 
+    `marca` VARCHAR(50), 
+    `estado` TINYINT(1) NOT NULL DEFAULT 1, 
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(), 
+    PRIMARY KEY (`id`) 
+); 
+
+ALTER TABLE `solicitudes_compras_detalles` 
+ADD FOREIGN KEY (`solicitud_id`) 
+REFERENCES `solicitudes_compras`(`id`) ON UPDATE CASCADE ON DELETE RESTRICT; 
